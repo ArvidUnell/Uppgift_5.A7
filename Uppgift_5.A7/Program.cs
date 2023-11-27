833aabc113705cead55ff1f6323a7f6f10d8e40f
ArvidUnell <arun06001@utb.vaxjo.se>
2023-11-27 19:26:31 +0100

    Add project files.

---
+++ Uppgift_5.A7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Uppgift_5_A7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] länder = { "1. Indien", "2. Kina", "3. USA", "4. Indonesien", "5. Pakistan" };
+
+            foreach (string land in länder)
+            {
+                Console.WriteLine(land);
+            }
+
+            Console.WriteLine("\nVilka länder vill du ska visas igen? Skriv deras nummer separerade med mellanslag.");
+
+            string[] namnArr;
+
+            while (true)
+            {
+                try
+                {
+                    string[] nrStr = Console.ReadLine().Split(' ');
+                    namnArr = new string[nrStr.Length];
+
+                    for (int i = 0; i < nrStr.Length; i++)
+                    {
+                        namnArr[i] = länder[byte.Parse(nrStr[i])];
+                    }
+
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Det är inte ett giltigt svar. Försök igen.");
+                }
+            }
+
+            foreach (string land in namnArr)
+            {
+                Console.WriteLine(land);
+            }
+
+            Console.WriteLine("\nTryck på valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
